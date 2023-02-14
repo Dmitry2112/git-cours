d70c533e20cd13f23068838c0a6a81b914b180ea
KazantsevaJane <103194481+KazantsevaJane@users.noreply.github.com>
2023-02-14 12:16:13 +0500

    new file

---
+++ jane.js
@@ -0,0 +1,17 @@
+document.body.innerHTML = '<h1>Helllllllo</h1>'
+console.log('Test !!!!!!!!!')
+console.log('Test !!!!!!!!!')
+console.log('Test !!!!!!!!!')
+function tarbumbum(a, b) {
+    return a + b;
+}
+
+console.log(tarbumbum())
+
+function kria(a) {
+    return a
+}
+
+function dbkria(a) {
+    return a * 2;
+}
