48793e3340df0f0219ce0dc5a1a04837228131ea
Dmitry <dmitriy_kozachenko_02@mail.ru>
2023-02-14 12:01:12 +0500

    added tarbumbum second popitka

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.body.innerHTML = '<h1>Helllllllo</h1>'
 console.log('Test !!!!!!!!!')
-<<<<<<< HEAD
-=======
 console.log('Test !!!!!!!!!')
 console.log('Test !!!!!!!!!')
->>>>>>> 42be85dba866932573a0714ec0c5569a34b52741
+function tarbumbum(a, b) {
+    return a + b;
+}
+
+console.log(tarbumbum())
